d0908f6874a3bbf031bc6c9eb1eb3d9f7217bee4
alix.brunsperger <alix.brunsperger@jobup.ch>
2018-09-30 07:04:11 +0200

    updating info with room address

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var render = function (template, selector) {
 
 const getHeader = (path) => (
     `<section class="top-image">
-        <img src="/assets/lotus-test-3-blanc.png" />
+        <a href="/"><img src="/assets/lotus-test-3-blanc.png" /></a>
     </section>
     <section class="title-text">
         Kung fu Sao Lim Hood Khar Pai Göteborg
@@ -17,11 +17,11 @@ const getHeader = (path) => (
             <img src="/assets/menu.png" onclick="toggleMenu()"/>
         </section>
         <section class="menu-content">
-            <a class="${ path == "/saolim"? "active" : "" }" href="/saolim.html">Presentation of the style</a>
-            <a class="${ path == "/lessons"? "active" : "" }" href="/lessons.html">Lessons</a>
-            <a class="${ path == "/teacher"? "active" : "" }" href="/teacher.html">Teacher</a>
-            <a class="${ path == "/schedules"? "active" : "" }" href="/schedules.html">Schedules and location</a>
-            <a class="${ path == "/inscription"? "active" : "" }" href="/inscription.html">Prices and inscription</a>
+            <a class="${ path == "/saolim"? "active" : "" }" href="/saolim">Presentation of the style</a>
+            <a class="${ path == "/lessons"? "active" : "" }" href="/lessons">Lessons</a>
+            <a class="${ path == "/teacher"? "active" : "" }" href="/teacher">Teacher</a>
+            <a class="${ path == "/schedules"? "active" : "" }" href="/schedules">Schedules and location</a>
+            <a class="${ path == "/inscription"? "active" : "" }" href="/inscription">Prices and inscription</a>
         </section>
     </section>`
 );
@@ -31,7 +31,6 @@ const loadPage = () => {
 }
 
 const toggleMenu = () => {
-    console.log(document.querySelector('.menu-content').classList);
     document.querySelector('.menu-icon').classList.toggle('active');
     document.querySelector('.menu-content').classList.toggle('active');
 }
